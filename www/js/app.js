3cd24d743106b3acc7cacb7e574940bfda095e33
Tania <Tania@iMac.local>
2015-07-18 00:49:14 +0200

    Changes

--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,6 @@ angular.module('fotografiasApp', ['ionic', 'fotografiasApp.controllers', 'fotogr
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
